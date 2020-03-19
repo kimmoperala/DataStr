77c52db73b85618fd0d87749fbf464c173da65de
kimmoperala <kimmo.perala@metropolia.fi>
2020-03-19 17:13:35 +0200

    New folder

---
+++ src/Tehtava1/Stack.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tehtava1;
+
+/**
+ *
+ * @author Kimmo Perälä
+ */
+public class Stack {
+    
+    private ListItem top;
+    private int size=0;
+    
+    // muodosta uusi lista-alkio ja vie se pinon huipulle
+    public void push(String aData){
+    	
+    	ListItem newItem = new ListItem();
+    	newItem.setData(aData);
+    	
+		if (top == null) {
+    		top = newItem;
+    	}
+		else {
+			ListItem last = top;
+			top = newItem;
+    		top.setNext(last);
+    	}
+    	this.size++;
+    }
+    // poista ja palauta alkio pinon huipulta, 
+    // jos pino tyhjä palauta null
+    public ListItem pop(){
+		ListItem last = top;
+
+    	if (top == null) {
+        	return null;
+    	} else {
+    		top = top.getNext();
+    		this.size--;
+    	}
+    	return last;
+    }
+    // tulosta pinon sisältö muuttamatta pinoa
+    public void printItems() {
+    	String result;
+    	if (top == null) {
+    		result = "Pino on tyhjä!";
+    	}
+    	else {
+    		result = "Pinossa on (isoin numero päällimmäinen):\n";
+    		result += size + ": " + top.getData();
+            ListItem nextItem = top.getNext();
+            for (int i = size-1; i>0; i--) {
+            	result +=", " + i + ": " + nextItem.getData();
+            	nextItem = nextItem.getNext();
+            }
+        }
+        System.out.println(result);
+    }
+    public int getSize() {
+        return this.size;
+    }
+    
+}
